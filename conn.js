14d1e1a8233b498360798292169dcb8b866e5669
PiyushSaini69 <piyushsaini1226@gmail.com>
2024-07-25 12:28:49 +0530

    Go

---
+++ conn.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://localhost:27017/Canteen');
+
+const con = mongoose.connection;
+con.on('open', () => {
+    console.log("connection success");
+})
+
+const formSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    confirmpassword: String
+})
+
+const Form = mongoose.model('Form',formSchema)
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type:String,
+        unique:true
+    },
+    password: String,
+    confirmpassword:String,
+})
+
+const User = mongoose.model('User',userSchema)
+
+export {Form,User}
+
+
